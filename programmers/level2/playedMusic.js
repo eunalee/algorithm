03ed6ac810f6e3576be546e1751972fad2706a9c
eunalee <luvlylunalee@gmail.com>
2021-07-25 16:29:54 +0900

    방금그곡 - 소스 수정

--- programmers/level2/playedMusic.js
+++ programmers/level2/playedMusic.js
@@ -2,6 +2,7 @@ let m = 'ABCDEFG';
 //let m = 'CC#BCC#BCC#BCC#B';
 //let m = 'ABC';
 let musicinfos = ['12:00,12:14,HELLO,CDEFGAB', '13:00,13:05,WORLD,ABCDEF'];
+//let musicinfos = ['12:59,13:00,HELLO,CDEFGAB', '13:00,13:14,WORLD,ABCDEF'];
 //let musicinfos = ['03:00,03:30,FOO,CC#B', '04:00,04:08,BAR,CC#BCC#BCC#B'];
 //let musicinfos = ['12:00,12:14,HELLO,C#DEFGAB', '13:00,13:05,WORLD,ABCDEF'];
 
@@ -67,10 +68,14 @@ function getPlayTime(start, end) {
     const endTime = end.split(':');
     
     // 재생시간(밀리초)
-    const duration = new Date(0, 0, endTime[0], endTime[1]).getTime() - new Date(0, 0, startTime[0], startTime[1]).getTime();
+    //const duration = new Date(0, 0, 1, endTime[0], endTime[1]).getTime() - new Date(0, 0, 1, startTime[0], startTime[1]).getTime();
+    const duration = new Date(0, 0, 0, endTime[0], endTime[1]).getTime() - new Date(0, 0, 0, startTime[0], startTime[1]).getTime();
+    /*const hour = endTime[0] - startTime[0];
+    const minute = endTime[1] - startTime[1];
+    const playTime = (hour * 60) + minute;*/
 
     // 밀리초 -> 분 으로 변환
-    const time = duration / (1000*60*60);
+    const time = duration / (1000*60) ;
 
     return time;
 }
@@ -78,11 +83,11 @@ function getPlayTime(start, end) {
 function getPlayCode(minute, codesInfo) {
     const codes = replaceCode(codesInfo);    // 악보코드 치환
     const count = codes.length;              // 악보코드 갯수
-
+    
     let code = '';
     if(minute > count) {
         // 재생시간 > 악보코드 갯수 : 악보코드 반복
-        code = codes.repeat(minute/count);
+        code = codes.repeat(minute/count) + codes.substr(0, (minute%count));
     } else {
         // 재생시간 < 악보코드 갯수 : 재생시간까지 악보코드 자르기
         code = codes.substr(0, minute);
