0c47824ed5dfcc8c795657274efa71294feab880
eunalee <luvlylunalee@gmail.com>
2021-08-26 23:17:46 +0900

    직업군 추천하기 - 소스 리팩토링

--- programmers/level1/recommendJob.js
+++ programmers/level1/recommendJob.js
@@ -1,44 +1,41 @@
 let table = ['SI JAVA JAVASCRIPT SQL PYTHON C#', 'CONTENTS JAVASCRIPT JAVA PYTHON SQL C++', 'HARDWARE C C++ PYTHON JAVA JAVASCRIPT', 'PORTAL JAVA JAVASCRIPT PYTHON KOTLIN PHP', 'GAME C++ C# JAVASCRIPT C JAVA'];
-let languages = ['PYTHON', 'C++', 'SQL'];
-//let languages = ['JAVA', 'JAVASCRIPT'];
-let preference = [7, 5, 5];
-//let preference = [7, 5];
+//let languages = ['PYTHON', 'C++', 'SQL'];
+let languages = ['JAVA', 'JAVASCRIPT'];
+//let preference = [7, 5, 5];
+let preference = [7, 5];
 
 solution(table, languages, preference);
 
 function solution(table, languages, preference) {
     var answer = '';
 
-    const score = [5,4,3,2,1];
-    const jobInfos = table.map(value => value.split(' '));
+    const jobInfos = table.map(value => value.split(' ').reverse());
 
-    //let totalResult = {};
     let result = [];
     jobInfos.forEach(job => {
         let total = 0;
-        languages.forEach((lang, i) => {
+        languages.forEach((lang, index) => {
             // 직업군 언어 목록에 입력받은 언어가 있는 경우에만 점수 계산
             if(job.includes(lang)) {
-                let index = job.indexOf(lang);
-                total += score[index-1] * preference[i];
+                let score = job.indexOf(lang);
+                total += (score+1) * preference[index];
             }
         });
 
-        //totalResult[job[0]] = total;
-        result.push({job: job[0], total : total});
+        result.push({job: job[5], total : total});
     });
 
     // 직업군명 사전순으로 정렬
-    const sortByJob = result.sort(function(a,b) {
+    result.sort(function(a,b) {
         return a.job < b.job ? -1 : a.job > b.job ? 1 : 0;
     });
 
     // 총합이 제일 높은순으로 정렬
-    const sortByTotal = sortByJob.sort(function(a,b) {
+    result.sort(function(a,b) {
         return b.total - a.total;
     });
 
-    answer = sortByTotal[0].job;
+    answer = result[0].job;
 
     return answer;
 }
