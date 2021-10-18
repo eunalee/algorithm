9934364b6ad8802e48dc06f7d2668e5ed40434c8
eunalee <luvlylunalee@gmail.com>
2021-10-18 23:38:38 +0900

    숫자 문자열과 영단어 - 다른 방법으로 풀이1

--- programmers/level1/changeWordsToNum.js
+++ programmers/level1/changeWordsToNum.js
@@ -7,19 +7,17 @@ let s = 'one4seveneight';
 solution(s);
 
 function solution(s) {
-  var answer = 0;
-  let replaceArr = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-
-  if (isNaN(s)) {
-    // 영단어 배열의 index 로 변환
-    replaceArr.forEach((value, index) => {
-      if (s.includes(value)) {
-        // 동일하게 반복되는 문자열 모두 숫자로 변환
-        s = s.replace(new RegExp(value, 'g'), index);
-      }
-    });
-  }
-
-  answer = Number(s);
-  return answer;
+  // 정규식으로 문자열 교체
+  s = s.replace(/zero/gi, 0)
+  .replace(/one/gi, 1)
+  .replace(/two/gi, 2)
+  .replace(/three/gi, 3)
+  .replace(/four/gi, 4)
+  .replace(/five/gi, 5)
+  .replace(/six/gi, 6)
+  .replace(/seven/gi, 7)
+  .replace(/eight/gi, 8)
+  .replace(/nine/gi, 9);
+  
+  return Number(s);
 }
