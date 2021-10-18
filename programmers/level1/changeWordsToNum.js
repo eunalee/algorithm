3756030fd031448e103ed8ded4b04f476483c1e6
eunalee <luvlylunalee@gmail.com>
2021-10-18 23:34:36 +0900

    숫자 문자열과 영단어

--- programmers/level1/changeWordsToNum.js
+++ programmers/level1/changeWordsToNum.js
@@ -11,8 +11,10 @@ function solution(s) {
   let replaceArr = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
 
   if (isNaN(s)) {
+    // 영단어 배열의 index 로 변환
     replaceArr.forEach((value, index) => {
       if (s.includes(value)) {
+        // 동일하게 반복되는 문자열 모두 숫자로 변환
         s = s.replace(new RegExp(value, 'g'), index);
       }
     });
